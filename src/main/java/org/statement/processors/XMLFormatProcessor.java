6ec0bdea9c2de56ef386d6f6aa3e1436ce1e23d3
Beerannanavar Tippeshappa <s.beerannanavartippeshappa@cgi.com>
2019-05-22 21:04:15 +0200

    fixing the xml parsing error

--- src/main/java/org/statement/processors/XMLFormatProcessor.java
+++ src/main/java/org/statement/processors/XMLFormatProcessor.java
@@ -1,6 +1,5 @@
 package org.statement.processors;
 
-import jdk.internal.org.xml.sax.SAXException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.statement.exception.FileParserException;
@@ -15,8 +14,14 @@ import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.File;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
@@ -37,11 +42,17 @@ public class XMLFormatProcessor implements IFormatProcessor {
             validateXML(fileAbsolutePath);
             JAXBContext jaxbContext = JAXBContext.newInstance(XMLRecords.class);
             Unmarshaller jaxbContextUnMarshaller = jaxbContext.createUnmarshaller();
-            xmlRecords = (XMLRecords)jaxbContextUnMarshaller.unmarshal(new File(fileAbsolutePath));
+            InputStream inputStream = null;
+            inputStream = new FileInputStream(fileAbsolutePath);
+            Reader reader = new InputStreamReader(inputStream, "UTF-8");
+            xmlRecords = (XMLRecords)jaxbContextUnMarshaller.unmarshal(reader);
 
-        } catch (JAXBException e) {
+        } catch (FileNotFoundException | JAXBException e) {
             LOGGER.error("Exception Occurred while unmarshaling " +e.getMessage());
             throw  new FileParserException("Error occurred while extracting data from :"+fileAbsolutePath);
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.error("Exception Occurred while unmarshaling " +e.getMessage());
+            throw  new FileParserException("Unsupported Encoding  :"+fileAbsolutePath);
         }
         return xmlRecords.getRecord();
     }
@@ -49,8 +60,7 @@ public class XMLFormatProcessor implements IFormatProcessor {
     private void validateXML(String fileAbsolutePath) throws FileParserException {
         SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         try {
-            Schema schema = schemaFactory.newSchema(new File(getResource(SCHEMA_FILE)));
-
+            Schema schema = schemaFactory.newSchema(getClass().getClassLoader().getResource(SCHEMA_FILE));
             Validator validator = schema.newValidator();
             validator.validate(new StreamSource(new File(fileAbsolutePath)));
 
